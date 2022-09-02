b079d90af085d050cee3e625ec8403e1d1ebaefe
Felix Taljaard <94836270+felixtaljaard@users.noreply.github.com>
2022-09-02 14:45:04 +0100

    delete everything

--- app/src/main/java/com/example/pacticemvvm/model/RetrofitServiceItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pacticemvvm.model
-
-data class RetrofitServiceItem(
-    val desc: String,
-    val image: String,
-    val title: String
-)
--- app/src/main/java/com/example/pacticemvvm/repository/MainRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pacticemvvm.repository
-
-import com.example.pacticemvvm.model.RetrofitServiceItem
-import com.example.pacticemvvm.network.RetrofitService
-
-class MainRepository constructor(private val retrofitService: RetrofitService) {
-    suspend fun getAllCakeAPI() = retrofitService.getCakeListService()
-}
